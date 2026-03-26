ae3f31c9fe2a787e436778c997d9ceff55d8dc10
ranjagit <ranjandas2378@gmail.com>
2026-03-26 12:06:54 +0530

    adde kong vars

--- ha/volumes/api/kong-entrypoint.sh
+++ ha/volumes/api/kong-entrypoint.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 set -e
 
-# Substitute environment variables in kong.yml
-envsubst < /home/kong/temp.yml > /usr/local/kong/kong.yml
+# Substitute environment variables manually (envsubst not available in kong image)
+sed \
+  -e "s|\${SUPABASE_ANON_KEY}|${SUPABASE_ANON_KEY}|g" \
+  -e "s|\${SUPABASE_SERVICE_KEY}|${SUPABASE_SERVICE_KEY}|g" \
+  -e "s|\${DASHBOARD_USERNAME}|${DASHBOARD_USERNAME}|g" \
+  -e "s|\${DASHBOARD_PASSWORD}|${DASHBOARD_PASSWORD}|g" \
+  /home/kong/temp.yml > /usr/local/kong/kong.yml
 
 exec /docker-entrypoint.sh kong docker-start
