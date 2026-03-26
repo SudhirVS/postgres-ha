b3c2c319a3ed5754b2b9df69bea957b30480b82f
ranjagit <ranjandas2378@gmail.com>
2026-03-26 12:45:52 +0530

    modifie file

--- ha/volumes/api/kong-entrypoint.sh
+++ ha/volumes/api/kong-entrypoint.sh
@@ -8,4 +8,8 @@ sed \
   -e "s|\${DASHBOARD_PASSWORD}|${DASHBOARD_PASSWORD}|g" \
   /home/kong/temp.yml > /usr/local/kong/kong.yml
 
-exec kong start --conf /usr/local/kong/kong.yml
+# Use 'kong start' then tail the logs to keep container alive
+kong start --conf /usr/local/kong/kong.yml
+
+# Keep container running by tailing kong logs
+exec tail -f /usr/local/kong/logs/error.log
